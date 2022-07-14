7d9c0e3930306c41806dd9a6f733045853f6172f
april <79454477+april-EXO@users.noreply.github.com>
2022-07-14 12:29:13 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-    <h1> HIIIIIII </h1>
+    <h1> AAAAAAA </h1>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
