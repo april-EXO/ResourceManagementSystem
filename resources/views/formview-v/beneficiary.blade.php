f3c892ec1b57c82c72a36e9115cf8454ca555c38
april <hsy.k10@gmail.com>
2022-07-27 01:40:42 +0800

    nothing

---
+++ resources/views/formview-v/beneficiary.blade.php
@@ -0,0 +1,104 @@
+<html>
+<head>
+<title>Resource Request</title>
+<h1>Resource Request 寻求物资</h1>
+<h2>Basic Infomation 基本资料</h2>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>Individual/ Organization name 个人/组织/机构 名称 :</th>
+            <td><input type="text" id="name" name="name"></td>
+        </tr>
+        <tr>
+            <th>ID(IC/ Entity registration number)  证件号码 :</th>
+            <td><input type="text" id="id" name="id"></td>
+        </tr>
+        <tr>
+            <th>State 州属 :</th>
+            <td>///
+            </td>
+            </tr>
+            <tr>
+            <th>Postcode 邮区编号 :</th>
+            <td>///
+            </td>
+            </tr>
+            <tr>
+            <th>Location 地区 :</th>
+            <td>///
+            </td>
+            </tr>
+            <tr>
+            <th>Contact Person 联络人 :</th>
+            <td>///
+            </td>
+            </tr>
+            <tr>
+            <th>Contact Number 联络号码 :</th>
+            <td>///
+            </td>
+            </tr>
+            <tr>
+            <th>Website 网站 :</th>
+            <td>///
+            </td>
+            </tr>
+            <tr>
+            <th>Social media page 社交媒体链接 :</th>
+            <td>///
+            </td>
+            </tr>
+
+            <tr>
+            <th>Resources Needed 所需物资</th>
+            </tr>
+            <tr>
+            <th><label for="money"> Monetary Contribution 金钱捐赠 :</th>
+            <td>///
+            </td>
+            </tr>
+            <tr>
+
+            <th><label for="goods"> Goods or products allocation 物资: </th>
+            <td>
+				///
+				<!-- <input type="checkbox" name="goodsFoodGroceries" value="1">Essential foods or daily groceries 每日粮食<br>
+				
+                <input type="checkbox" name="goodsFurnitureFixing" value="1">Furniture and fixing 家具配备<br>
+				
+                <input type="checkbox" name="goodsElectricalItemsGadgets" value="1"> Electrical items and gadgets 家电 (eg. TV, internet, computer, etc. 例: 电视，网络，电脑，等)<br>
+				
+                <input type="checkbox" name="goodsTranspotation" value="1">Transportation 运输 (eg. wheelchair, vehicle, trolley, etc. 例: 轮椅，车辆，手推车，等)<br>
+				
+                <input type="checkbox" name="goodsEducationalMaterial" value="1">Educational materials 教育产品 (eg. books, magazine, etc. 例：书籍，杂志，等)<br>
+				
+				<input type="checkbox" name="other1" id="box" value="1">Other: <input type="text" name="otherGoods" id="content" disabled><br> -->
+            </td>
+            </tr>
+            <tr>
+            <th>Services 服务: </th>
+            <td>
+            ///
+				<!-- <input type="checkbox" name="cleaning" value="1">Cleaning 清洗<br>
+				
+                <input type="checkbox" name="moving" value="1">Moving 搬运<br>
+				
+                <input type="checkbox" name="education" value="1"> Education 教育<br>
+				
+				<input type="checkbox" name="other2" id="box" value="1">Other: <input type="text" name="otherServices" id="content" disabled><br> -->
+            </td>
+            </tr>
+            <tr>
+            <th><label for="message"> Comment/ Message 反馈/留言 :</th>
+            <td>///
+            </td>
+            </tr>
+            <tr>
+                </td>
+            </tr>
+    </table>
+</body>
+
+</html>
