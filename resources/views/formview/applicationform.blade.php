1e2f09b572c9e200f2e09555316b4227e532cc74
april <hsy.k10@gmail.com>
2022-07-27 10:33:40 +0800

    Revert "Revert "Revert "Revert "nothing""""
    This reverts commit 24707f5bcd249c280ca0ec4ddc92fcaaae3a947c.

---
+++ resources/views/formview/applicationform.blade.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+    <title>
+        Application for beneficiary/connector/contributor/volunteer
+    </title>
+    <h1>Application for beneficiary/connector/contributor/volunteer</h1>
+    <h2>申请 - 受益者/召唤活动志愿者/贡献者/志愿者</h2>
+    </head>
+    <body>
+    <a href="/form/beneficiary">Apply for help 申请援助</a> <br>
+    <a href="/form/connector">Apply for volunteer helps 召唤志愿者</a> <br>
+    <a href="/form/contributor">Join as contributor 成为贡献者</a> <br>
+    <a href="/form/volunteer">Join as volunteer 成为志愿者</a> <br><br><br><br>
+
+
+    <a href="/form/beneficiary-v">View beneficiary</a> <br>
+    <a href="/form/connector-v">View connector</a> <br>
+    <a href="/form/contributor-v">View contributor</a> <br>
+    <a href="/form/volunteer-v">View volunteer</a> <br>
+    </body>
+</html>
