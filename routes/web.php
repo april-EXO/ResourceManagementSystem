b7b5c6b27558c0b56e514fbfaf201c7fd620e8ae
april <hsy.k10@gmail.com>
2022-07-27 10:18:39 +0800

    Revert "nothing"
    This reverts commit f3c892ec1b57c82c72a36e9115cf8454ca555c38.

--- routes/web.php
+++ routes/web.php
@@ -21,19 +21,11 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
-Route::get('/form/application', function () {
-    return view('formview.applicationform');
-});
-
 Route::get('/form/beneficiary', function () {
     return view('formview.beneficiary');
 });
 
 Route::post('/form/beneficiary', [FormController::class, 'addBeneficiary']);
-Route::get('/form/beneficiary-v', function () {
-    return view('formview-v.beneficiary');
-});
 
 Route::get('/form/contributor', function () {
     return view('formview.contributor');
@@ -41,11 +33,14 @@ Route::get('/form/contributor', function () {
 
 Route::post('/form/contributor', [FormController::class, 'addContributor']);
 
+
+
 Route::get('/form/volunteer', function () {
     return view('formview.volunteer');
 });
 
 Route::post('/form/volunteer', [FormController::class, 'addVolunteer']);
+
 
 Route::get('/form/connector', function () {
     return view('formview.connector');
