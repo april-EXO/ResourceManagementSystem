1c43601f64c8219386b6f9c26910f143d1311b00
xueen0906 <xueen2000@1utar.my>
2022-08-03 00:56:01 +0800

    carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,14 +8,15 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
     <title>Resource Management System</title>
 </head>
 
 <body>
+    <!-- Navigation Bar-->
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">AppName</a>
+            <a class="navbar-brand" href="#">RMS</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -48,6 +49,54 @@
             </div>
         </div>
     </nav>
+    <!-- Carousel Banner-->
+    <div id="carouselMainPage" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselMainPage" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselMainPage" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselMainPage" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{asset("images/Old-folks-home.jpg")}}" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Do a Good Deed, Give to Those in Need.</h5>
+                    <p>Donate food to our welfare centre today!</p>
+                    <div class="slider-btn">
+                        <button class="btn btn-1">Donate Now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="{{asset("images/Beneficiary.jpg")}}" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Need Resource Support? We Can Help.</h5>
+                    <p>Become a beneficiary to receive resources support.</p>
+                    <div class="slider-btn">
+                        <button class="btn btn-2">More Info</button>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="{{asset("images/Volunteer.jpg")}}" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Volunteer to Aid Us with Our Charitable Causes.</h5>
+                    <p>Volunteer today to help us achieve more!</p>
+                    <div class="slider-btn">
+                        <button class="btn btn-3">More Info</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
